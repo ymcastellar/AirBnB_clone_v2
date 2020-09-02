78aa53350c2fb90f1f742718444545939dd9ed7f
ymcastellar <yoycas@hotmail.com>
2020-09-02 16:14:53 -0500

    files for task8 added

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" starts a Flask web application """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def index():
+    """display the states and cities """
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """display /cities_by_states """
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(db):
+    """close db"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(port='5000', debug=True, host='0.0.0.0')
