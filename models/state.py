02227253917a14dfafea9ac714aabc3ba8374f26
ymcastellar <yoycas@hotmail.com>
2020-09-02 12:42:30 -0500

    file for task 7 added

--- models/state.py
+++ models/state.py
@@ -11,10 +11,9 @@ from os import getenv
 class State(BaseModel, Base):
     """State class."""
 
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-
     if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state",
                               cascade="all, delete-orphan")
 
